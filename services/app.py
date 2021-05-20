62e9337d5c6368afdfce2086f98cf175ee661eeb
Donggeun Lee <piou987@gmail.com>
2021-05-20 16:18:48 +0900

    Add ec2 instance

--- services/app.py
+++ services/app.py
@@ -16,6 +16,8 @@ from security.iam.iam_stack import IamStack
 from security.nacl.nacl_stack import NaclStack
 from security.security_group.security_group_stack import SecurityGroupStack
 from eks.eks_stack import EksStack
+from ec2.instance_stack import EC2InstanceStack
+from elb.elb_stack import ElasticLoadBalancerStack
 
 # Information of project
 project = dict()
@@ -24,6 +26,7 @@ project['region']  = "us-east-1"
 project['env']     = "dev"
 project['name']    = "cdkworkshop"
 project['prefix']  = f"{project['env']}-{project['name']}"
+project['keypair'] = "dev-useast1"
 
 # cdk environment
 cdk_environment = core.Environment(
@@ -71,6 +74,14 @@ nacl_stack = NaclStack(
     construct_id   = f"{project['prefix']}-nacl",
     project        = project,
     vpc            = vpc_stack.vpc,
+    env            = cdk_environment)
+
+ec2_instance_stack = EC2InstanceStack(
+    scope          = app,
+    construct_id   = f"{project['prefix']}-ec2-instance",
+    project        = project,
+    vpc            = vpc_stack.vpc,
+    security_group = security_group_stack.security_group,
     env            = cdk_environment)
 
 # app synth -> cloudformation template
---
+++ services/ec2/instance_stack.py
@@ -0,0 +1,86 @@
+'''
+    VPC와 Security Group에 의존적이다.
+    Instance profile은 해당 스택 내부에서 별도로 만들어주는 게 좋다
+'''
+from aws_cdk import (
+    core, aws_iam, aws_ec2
+)
+
+class EC2InstanceStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, project: dict, vpc, security_group, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Initial
+        self.project        = project
+        self.vpc            = vpc
+        self.security_group = security_group
+        self.create_security_group() # 원래는 security-group stack에서 생성함
+
+        # Create Role
+        self.role = dict()
+        self.role['foo-app'] = aws_iam.Role(self, "role-foo-app",
+            role_name   = f"{self.project['prefix']}-role-foo-app",
+            description = "",
+            assumed_by  = aws_iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM")
+            ])
+
+        # AMI
+        # ami = aws_ec2.MachineImage.from_ssm_parameter("/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2")
+        ami = aws_ec2.MachineImage.latest_amazon_linux(
+            generation=aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=aws_ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=aws_ec2.AmazonLinuxVirt.HVM,
+            storage=aws_ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+            cpu_type=aws_ec2.AmazonLinuxCpuType.X86_64)
+        # ami = aws_ec2.MachineImage.genericLinux({
+        #     'us-east-1': 'ami-97785bed',
+        #     'eu-west-1': 'ami-12345678',
+        #     })
+
+        # EC2 Instance
+        aws_ec2.Instance(self, "ec2-foo-app",
+            instance_type=aws_ec2.InstanceType("t3.xlarge"),
+            machine_image=ami,
+            vpc=self.vpc,
+            allow_all_outbound=None,
+            availability_zone=None,
+            block_devices=[
+                aws_ec2.BlockDevice(
+                    device_name="/dev/xvda",
+                    volume=aws_ec2.BlockDeviceVolume.ebs(
+                        volume_type=aws_ec2.EbsDeviceVolumeType.GP3,
+                        # iops=3000,
+                        volume_size=20,
+                        encrypted=True,
+                        delete_on_termination=True,
+                    )
+                )
+            ],
+            init=None,
+            init_options=None,
+            instance_name=f"{self.project['prefix']}-ec2-foo-app",
+            key_name=self.project['keypair'],
+            private_ip_address=None,
+            resource_signal_timeout=None,
+            role=self.role['foo-app'],
+            security_group=self.security_group['foo-app'],
+            source_dest_check=None,
+            user_data=None,
+            user_data_causes_replacement=None,
+            vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE))
+
+
+    def create_security_group(self):
+        # 원래는 security group stack에서 별도로 생성해준다.
+        # ㅇㅇ
+        self.security_group['foo-app'] = aws_ec2.SecurityGroup(self, 'sg-foo-app',
+            vpc                 = self.vpc,
+            security_group_name = f"{self.project['prefix']}-sg-foo-app",
+            description         = "",
+            allow_all_outbound  = True)
+        core.Tags.of(self.security_group['foo-app']).add(
+            "Name",
+            f"{self.project['prefix']}-sg-foo-app")
