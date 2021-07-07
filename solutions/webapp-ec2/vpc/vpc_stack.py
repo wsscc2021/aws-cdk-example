952a96d9b1ac51cb5bf446f2a9a0f614b02d8810
Donggeun Lee <piou987@gmail.com>
2021-07-07 15:39:11 +0900

    Modfiy solutions webapp-ec2, add vpc endpoint

--- solutions/webapp-ec2/vpc/vpc_stack.py
+++ solutions/webapp-ec2/vpc/vpc_stack.py
@@ -7,6 +7,10 @@ class VpcStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, project: dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # init
+        self.project = project
+        
+        # resource
         self.vpc = aws_ec2.Vpc(self, "vpc",
             cidr="10.0.0.0/16",
             max_azs=3,
@@ -31,3 +35,16 @@ class VpcStack(core.Stack):
         )
 
         self.vpc.add_flow_log(id=f"{project['prefix']}-vpc-flow-log")
+        self.add_vpc_endpoint()
+    
+    def add_vpc_endpoint(self):
+        # vpc endpoint
+        # Gateway
+        vpc_endpoint = aws_ec2.GatewayVpcEndpoint(self, "vpc-endpoint-s3-gateway",
+            vpc=self.vpc,
+            # https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/InterfaceVpcEndpointAwsService.html
+            service=aws_ec2.GatewayVpcEndpointAwsService.S3,
+            subnets=self.vpc.private_subnets)
+        core.Tags.of(vpc_endpoint).add(
+            "Name",
+            f"{self.project['prefix']}-s3gateway-vpce")
