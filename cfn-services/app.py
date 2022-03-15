a9fc3e2b98d23477ad416db4abfe1f5576a0575a
Donggeun Lee <piou987@gmail.com>
2022-03-15 10:17:07 +0900

    Feat: add CFN example of security-group

--- cfn-services/app.py
+++ cfn-services/app.py
@@ -6,6 +6,7 @@ from aws_cdk import (
 
 # Import service stack modules
 from vpc import VPCStack
+from security.security_group import SecurityGroupStack
 
 # CDK App
 app = App()
@@ -14,6 +15,11 @@ app = App()
 vpcStack = VPCStack(
     scope        = app,
     construct_id = "vpc")
+
+securityGroupStack = SecurityGroupStack(
+    scope        = app,
+    construct_id = "security-group",
+    vpc          = vpcStack.vpc)
 
 # Synthesize
 app.synth()
---
+++ cfn-services/security/security_group.py
@@ -0,0 +1,105 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    CfnTag,
+    aws_ec2
+)
+
+class SecurityGroupStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, vpc, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        # init
+        self.security_group = dict()
+
+        # security group
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/CfnSecurityGroup.html
+        self.security_group["product-ext"] = aws_ec2.CfnSecurityGroup(self,
+            "product-ext-sg",
+            group_name="product-ext-sg",
+            group_description="description",
+            security_group_egress=None,
+            security_group_ingress=None,
+            vpc_id=vpc.ref,
+            tags=[
+                CfnTag(
+                    key="Name",
+                    value="product-ext-sg"
+                )
+            ])
+        self.security_group["product-api"] = aws_ec2.CfnSecurityGroup(self,
+            "product-api-sg",
+            group_name="product-api-sg",
+            group_description="description",
+            security_group_egress=None,
+            security_group_ingress=None,
+            vpc_id=vpc.ref,
+            tags=[
+                CfnTag(
+                    key="Name",
+                    value="product-api-sg"
+                )
+            ])
+        self.security_group["bastion"] = aws_ec2.CfnSecurityGroup(self,
+            "bastion-sg",
+            group_name="bastion-sg",
+            group_description="description",
+            security_group_egress=None,
+            security_group_ingress=None,
+            vpc_id=vpc.ref,
+            tags=[
+                CfnTag(
+                    key="Name",
+                    value="bastion-sg"
+                )
+            ])
+        
+        # security group ingress
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/CfnSecurityGroupIngress.html
+        # security group egress
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/CfnSecurityGroupEgress.html
+        aws_ec2.CfnSecurityGroupIngress(self, "product-ext-sg-r1",
+            ip_protocol="tcp", # tcp , udp , icmp , icmpv6 , -1 (all)
+            cidr_ip="0.0.0.0/0",
+            from_port=80, # -1 (all)
+            to_port=80, # -1 (all)
+            group_id=self.security_group["product-ext"].ref,
+            group_name=None,
+            source_prefix_list_id=None,
+            source_security_group_id=None,
+            source_security_group_name=None,
+            source_security_group_owner_id=None,)
+        
+        aws_ec2.CfnSecurityGroupEgress(self, "product-ext-sg-r2",
+            ip_protocol="tcp",  # tcp , udp , icmp , icmpv6 , -1 (all)
+            cidr_ip=None,
+            from_port=8080, # -1 (all)
+            to_port=8080, # -1 (all)
+            group_id=self.security_group["product-ext"].ref,
+            destination_prefix_list_id=None,
+            destination_security_group_id=self.security_group["product-api"].ref,)
+
+        aws_ec2.CfnSecurityGroupIngress(self, "product-api-sg-r1",
+            ip_protocol="tcp", # tcp , udp , icmp , icmpv6 , -1 (all)
+            cidr_ip=None,
+            from_port=8080, # -1 (all)
+            to_port=8080, # -1 (all)
+            group_id=self.security_group["product-api"].ref,
+            group_name=None,
+            source_prefix_list_id=None,
+            source_security_group_id=self.security_group["product-ext"].ref,
+            source_security_group_name=None,
+            source_security_group_owner_id=None,)
+        
+        aws_ec2.CfnSecurityGroupIngress(self, "bastion-sg-r1",
+            ip_protocol="tcp", # tcp , udp , icmp , icmpv6 , -1 (all)
+            cidr_ip="175.195.57.38/32",
+            from_port=22, # -1 (all)
+            to_port=22, # -1 (all)
+            group_id=self.security_group["bastion"].ref,
+            group_name=None,
+            source_prefix_list_id=None,
+            source_security_group_id=None,
+            source_security_group_name=None,
+            source_security_group_owner_id=None,)
